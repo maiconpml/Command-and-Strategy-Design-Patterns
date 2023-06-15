94c093ec52fb6f9f52aa6f7c34c197cfd2a5893e
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2023-06-14 23:01:46 -0300

    Create Controller.java

---
+++ CommandDesignPattern/src/main/java/br/edu/ifnmg/client/objects/Controller.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.client.objects;
+
+import br.edu.ifnmg.client.command.ICommand;
+import java.util.Stack;
+
+/**
+ *
+ * @author maicon
+ */
+public class Controller {
+
+    private ICommand button1;
+    private ICommand button2;
+    private ICommand button3;
+    private ICommand button4;
+    private Stack<ICommand> history;
+    
+    public Controller(ICommand button1, ICommand button2, ICommand button3, ICommand button4) {
+        this.button1 = button1;
+        this.button2 = button2;
+        this.button3 = button3;
+        this.button4 = button4;
+        history = new Stack();
+    }
+
+    public void setButton1(ICommand button1) {
+        this.button1 = button1;
+    }
+
+    public void setButton2(ICommand button2) {
+        this.button2 = button2;
+    }
+
+    public void setButton3(ICommand button3) {
+        this.button3 = button3;
+    }
+
+    public void setButton4(ICommand button4) {
+        this.button4 = button4;
+    }
+
+    public ICommand getButton1() {
+        return button1;
+    }
+
+    public ICommand getButton2() {
+        return button2;
+    }
+
+    public ICommand getButton3() {
+        return button3;
+    }
+
+    public ICommand getButton4() {
+        return button4;
+    }
+
+    public void clickButton1() {
+        history.push(button1);
+        button1.execute();
+    }
+
+    public void clickButton2() {
+        history.push(button2);
+        button2.execute();
+    }
+
+    public void clickButton3() {
+        history.push(button3);
+        button3.execute();
+    }
+
+    public void clickButton4() {
+        history.push(button4);
+        button4.execute();
+    }
+    
+    public void undoOperation(){
+        if(history.empty()) return;
+        
+        history.pop().unexecute();
+    }
+
+}
