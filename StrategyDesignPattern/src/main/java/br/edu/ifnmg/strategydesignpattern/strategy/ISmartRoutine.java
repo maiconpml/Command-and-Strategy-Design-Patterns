b089d0336c135a6950275f3860558222e01a0dd5
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2023-06-14 23:02:38 -0300

    Create ISmartRoutine.java

---
+++ StrategyDesignPattern/src/main/java/br/edu/ifnmg/strategydesignpattern/strategy/ISmartRoutine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.ifnmg.strategydesignpattern.strategy;
+
+import br.edu.ifnmg.strategydesignpattern.control.Room;
+
+/**
+ *
+ * @author maicon
+ */
+public interface ISmartRoutine {
+    
+    void execute(Room room); 
+}
