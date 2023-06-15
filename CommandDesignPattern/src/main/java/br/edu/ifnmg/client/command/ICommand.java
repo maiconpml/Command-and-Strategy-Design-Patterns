8ca4ccd9f125323a66a1f3cf5f8bea8c3db72078
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2023-06-14 22:59:45 -0300

    Create ICommand.java

---
+++ CommandDesignPattern/src/main/java/br/edu/ifnmg/client/command/ICommand.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.edu.ifnmg.client.command;
+
+/**
+ *
+ * @author maicon
+ */
+public interface ICommand {
+    
+    void execute();
+    
+    void unexecute();
+}
