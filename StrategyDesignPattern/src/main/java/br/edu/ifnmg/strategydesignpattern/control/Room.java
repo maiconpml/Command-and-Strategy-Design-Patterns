5a68b7bc4ba5ec2131e5c7e1cb84b9b823bf1934
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2023-06-14 23:02:32 -0300

    Create Room.java

---
+++ StrategyDesignPattern/src/main/java/br/edu/ifnmg/strategydesignpattern/control/Room.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.strategydesignpattern.control;
+
+/**
+ *
+ * @author maicon
+ */
+public class Room {
+    
+    private final String name;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }    
+}
