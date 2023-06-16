f0cfd3eb3c0d4b42224db05c0b0c2f505f2ede5b
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2023-06-15 22:22:38 -0300

    Update Client.java

--- CommandDesignPattern/src/main/java/br/edu/ifnmg/client/Client.java
+++ CommandDesignPattern/src/main/java/br/edu/ifnmg/client/Client.java
@@ -1,13 +1,12 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package br.edu.ifnmg.client;
 
-import br.edu.ifnmg.client.command.RaiseLightWarmthCommand;
-import br.edu.ifnmg.client.command.ReduceLightWarmthCommand;
-import br.edu.ifnmg.client.command.TurnOffLightCommand;
-import br.edu.ifnmg.client.command.TurnOnLightCommand;
+import br.edu.ifnmg.client.command.RaiseLampWarmthCommand;
+import br.edu.ifnmg.client.command.ReduceLampWarmthCommand;
+import br.edu.ifnmg.client.command.TurnOffLampCommand;
+import br.edu.ifnmg.client.command.TurnOnLampCommand;
 import br.edu.ifnmg.client.objects.Controller;
 import br.edu.ifnmg.client.objects.Lamp;
 
@@ -18,28 +17,29 @@ import br.edu.ifnmg.client.objects.Lamp;
 public class Client {
 
     public static void main(String[] args) {
+
+        var lamp1 = new Lamp("CeilBedroomLight", 4000, false);
+
+        var bedroomLightsController = new Controller(new TurnOnLampCommand(lamp1),
+                new TurnOffLampCommand(lamp1),
+                new RaiseLampWarmthCommand(lamp1));
+
+        bedroomLightsController.clickButton1();//turn on lamp1
+        System.out.println(lamp1);
         
-        var light1 = new Lamp("CeilBedroomLight", 4000, false);
+        bedroomLightsController.clickButton2();//turn off lamp1
+        System.out.println(lamp1);
         
-        var bedroomLightsController = new Controller(new TurnOnLightCommand(light1), new TurnOffLightCommand(light1), new RaiseLightWarmthCommand(light1), new ReduceLightWarmthCommand(light1));
+        bedroomLightsController.clickButton1();//turn on lamp1
+        bedroomLightsController.clickButton3();//raise warmth of lamp1
+        bedroomLightsController.clickButton3();// --
+        bedroomLightsController.clickButton3();// --
+        bedroomLightsController.clickButton3();// -- 
+        bedroomLightsController.clickButton3();//raise warmth of lamp1
+        System.out.println(lamp1);
         
-        bedroomLightsController.clickButton1();
-        System.out.println(light1);
-        bedroomLightsController.clickButton2();
-        System.out.println(light1);
-        bedroomLightsController.clickButton1();
-        bedroomLightsController.clickButton3();
-        bedroomLightsController.clickButton3();
-        bedroomLightsController.clickButton3();
-        bedroomLightsController.clickButton3();
-        bedroomLightsController.clickButton3();
-        System.out.println(light1);
-        bedroomLightsController.clickButton4();
-        bedroomLightsController.clickButton4();
-        bedroomLightsController.clickButton4();
-        System.out.println(light1);
-        bedroomLightsController.undoOperation();
-        bedroomLightsController.undoOperation();
-        System.out.println(light1);
+        bedroomLightsController.undoButton();//undo last raise warmth
+        bedroomLightsController.undoButton();//undo last raise warmth
+        System.out.println(lamp1);
     }
 }
